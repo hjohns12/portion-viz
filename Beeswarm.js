634eaab0a19c29655f8f37eff25654c496359d9c
Hope Johnson <hopecaneel@gmail.com>
2020-04-01 11:54:02 -0400

    change margins to reveal axis labels

--- Beeswarm.js
+++ Beeswarm.js
@@ -4,7 +4,7 @@ class Beeswarm {
       // initialize properties here
       this.width = window.innerWidth * 0.6;
       this.height = window.innerHeight * 0.6;
-      this.margins = { top: 20, bottom: 20, left: 20, right: 20 };
+      this.margins = { top: 20, bottom: 50, left: 20, right: 20 };
 
       this.svg = d3
         .select("#beeswarm")
