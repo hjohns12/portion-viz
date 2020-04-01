98372e46d954011d9ad9cc1c576a49114c0bf661
Hope Johnson <hopecaneel@gmail.com>
2020-04-01 19:33:20 -0400

    working boxplotahhhhh

--- Boxplot.js
+++ Boxplot.js
@@ -1,8 +1,8 @@
 class Boxplot {
 
     constructor(state, setGlobalState) {
-        this.width = window.innerWidth * 0.6
-        this.height = window.innerHeight * 0.6
+        this.width = window.innerWidth * 0.9
+        this.height = window.innerHeight * 0.9
         this.margins = {top: 20, bottom:100, left:60, right:20};
     
         this.svg = d3
@@ -11,20 +11,20 @@ class Boxplot {
           .attr("width", this.width)
           .attr("height", this.height);
 
-        this.districts = ["1", "2", "3", "4", "5", "6", "7", "8"]
-        
+        this.districts = ["District 1", "District 2", "District 3", "District 4", "District 5", "District 6", "District 7", "District 8"]
     }
 
     draw(state, setGlobalState) {
+
         const bins = d3.histogram()
           .thresholds([1, 2, 3, 4, 5, 6, 7, 8]) //not sure about numeric versus string
         // .thresholds(this.districts)
         //   .domain(this.xScale.domain)
-          .value(d => d.District)
+          .value(d => parseInt(d.District))
         (state.long_data)
           .map(bin => {
-            //   bin.sort((a, b) => a.Value - b.Value);
-              bin.sort((a, b) => d3.ascending(a.District, b.District))
+              bin.sort((a, b) => a.Value - b.Value);
+            //   bin.sort((a, b) => d3.ascending(a.District, b.District))
               const values = bin.map(d => d.Value);
               const min = values[0];
               const max = values[values.length - 1];
@@ -37,20 +37,23 @@ class Boxplot {
               bin.quartiles = [q1, q2, q3];
               bin.range = [bin.r0, bin.r1];
               bin.outliers = bin.filter(v => v.Value < bin.r0 || v.Value > bin.r1); // TODO
+              bin.district = "District " + bin.x0;
+              bin.distNum = bin.x0;
               return bin;
             })
         console.log("bins", bins)
         
         // set up the scales
         const xScale = d3
-          .scaleLinear()
-          .domain([d3.min(bins, d => d.x0), d3.max(bins, d => d.x1)])
-          .rangeRound([this.margins.left, this.width - this.margins.right])
+            .scaleBand()
+            .domain(this.districts)
+            .rangeRound([this.margins.left, this.width - this.margins.right])
+            .paddingInner(0.3);
 
         const yScale = d3
             .scaleLinear()
             // .domain([d3.min(bins, d =>  d.range[0]), d3.max(bins, d => d.range[1])]).nice()
-            .domain(state.domain)
+            .domain([0, d3.max(state.long_data, d => d.Value)])
             .range([this.height - this.margins.bottom, this.margins.top])
         
         // make x-axis
@@ -79,7 +82,49 @@ class Boxplot {
             .attr("dx", "-3em")
             .attr("writing-mode", "vertical-rl")
             .text("D voteshare");
-            
+        
+        // make boxes
+        const g = this.svg.append("g")
+            .selectAll("g")
+            .data(bins)
+            .join("g");
+        
+        // vertical line
+        g.append("line")
+            .attr("stroke", "black")
+            .attr('x1', d => xScale(d.district) + xScale.bandwidth()/2)
+            .attr('x2', d => xScale(d.district) + xScale.bandwidth()/2)
+            .attr('y1', d => yScale(d.r1))
+            .attr('y2', d => yScale(d.r0))
+
+        // box 
+        g.append("rect")
+            .attr("y", d => yScale(d.quartiles[2]))
+            .attr("x", d => xScale(d.district))
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => yScale(d.quartiles[0]) - yScale(d.quartiles[2]))
+            .attr("fill", "#ddd")
+
+        // median line
+        g.append("line")
+            .attr("stroke", "black")
+            .attr('x1', d => xScale(d.district))
+            .attr('x2', d => xScale(d.district) + xScale.bandwidth())
+            .attr('y1', d => yScale(d.quartiles[1]))
+            .attr('y2', d => yScale(d.quartiles[1]))
+
+        // outlier dots 
+        g.append("g")
+            .attr("fill", "currentColor")
+            .attr("fill-opacity", 0.2)
+            .attr("stroke", "none")
+            .attr("transform", d => `translate(${xScale(d.district) + xScale.bandwidth()/2},0)`)
+            .selectAll("circle")
+            .data(d => d.outliers)
+            .join("circle")
+            .attr("r", 3)
+            .attr("cx", () => (Math.random() - 0.5) * 4)
+            .attr("cy", d => yScale(d.Value));
 
 
     }
