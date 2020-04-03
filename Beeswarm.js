deea215a22cfe1ea620283eb73478a4317f5dd83
Hope Johnson <hopecaneel@gmail.com>
2020-04-03 17:42:38 -0400

    update with wider

--- Beeswarm.js
+++ Beeswarm.js
@@ -2,7 +2,7 @@ class Beeswarm {
 
     constructor(state, setGlobalState) {
       // initialize properties here
-      this.width = window.innerWidth * 0.6;
+      this.width = window.innerWidth * 0.9
       this.height = window.innerHeight * 0.6;
       this.margins = { top: 20, bottom: 50, left: 20, right: 20 };
 
