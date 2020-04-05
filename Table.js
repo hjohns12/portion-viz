10cabee14c6269a5e2d9e465a2ffe7db37cb3489
Hope Johnson <hopecaneel@gmail.com>
2020-04-05 17:51:45 -0400

    add working table

--- Table.js
+++ Table.js
@@ -12,42 +12,43 @@ class Table {
           .join("th")
           .text(d => d);
 
-        // make this a "this" to invoke global scope
-        this.tableRows = this.table
-        .append("tbody")
-        .selectAll("tr")
-        .data(["", "", ""])
-        .join("tr")
-    //  .style("background-color", d => this.colorScale(d['Total Housing Units']))
-    //  .style("background-color", 'gray')
-    //  .style("color", "#eee");
-
-
     }
 
     draw(state) {
         const formatValue = d3.format(".3");
 
         if (state.clickedData !== null) {
-
+            
             const selectData = [state.clickedData].map(d => ({
                 "D Seats": +d.D_seats,
                 "R Seats": 8 - +d.D_seats,
                 "EG": formatValue(d.eg),
             }))
-            console.log("selectData", selectData);            
+            
+            // this.table
+            //   .selectAll("table.child")
+            //   .data(selectData)
+            //   .join(enter => enter
+            //     // .append("tbody") //or should this be tr?
+            //     .call(sel => sel.append("tr")
+            //     .join("td")
+            //     .text(d => Object.values(d))
+            //     ),
+            //     update => update,
+            //     exit => exit.remove()
+            //     );
 
             this.tableRows = this.table
-            .append("tbody")
-            .selectAll("tr")
-            .data(selectData)
-            .join("tr")
+              .append("tbody")
+              .selectAll("tr")
+              .data(selectData)
+              .join("tr")
 
            this.tableRows
-             .selectAll("td")
-             .data(d => Object.values(d))
-             .join("td")
-             .text(d => d);
+              .selectAll("td")
+              .data(d => Object.values(d))
+              .join("td")
+              .text(d => d);
         }
 
     }
