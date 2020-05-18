dc1fb1d5130389c841dfb5171ebb53ca59b89d2a
Hope Johnson <hopecaneel@gmail.com>
2020-05-18 15:37:56 -0400

    fix typo

--- Explainer.js
+++ Explainer.js
@@ -5,11 +5,11 @@ class Explainer {
              <span class="fancy-text">click on a dot from either graph</span> to observe its corresponding value in the other.`)
              .append("p")
             .text(
-            `Every dot represents a possible congressional redistricting plan for the state of Missouri. I 
+            `Every dot represents a possible state senate redistricting plan for the state of Missouri. I 
             generated one million maps using Markov Chain Monte Carlo sampling. 
             I randomly selected 1,000 plans from each ensemble for these graphs.`)
             .append("p")
-            .html(`The efficiency gap is a method for measuring partisan gerrymanding. Anything <span class="fancy-text">above 0.7 or below -0.7 is 
+            .html(`The efficiency gap is a method for measuring partisan gerrymandering. Anything <span class="fancy-text">above 0.7 or below -0.7 is 
             considered extreme.</span>`)
 
     }
